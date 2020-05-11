94394400647821f8480ea0cd54e34201facee483
n0bis <madsfalken@Madss-MacBook-Pro.local>
2020-05-11 17:15:51 +0200

    added comments

--- Element.py
+++ Element.py
@@ -1,3 +1,31 @@
+""""
+    At lave dit eget værktøj til at komprimere filer.
+    Komprimeringen skal ske via Huffman-kodning. Der skal laves to
+    programmer: et til at kode/komprimere en fil, og et til dekode den igen.
+
+    Opgave 1:
+        I opgave 1 skal man bruge kaldet read(1) fra file objects til
+        at læse bytes fra inputfilen (den originale fil). Man skal bruge metoderne writeint32bits(intvalue) og writebit(bit) fra klassen BitWriter i biblioteket bitIO.py til at skrive heltal (for hyppighedstabel) og bits (for Huffmans-koderne) til outputfilen (den
+        komprimerede fil). Begge filer skal åbnes i “binary mode”. Når en
+        BitWriter instantieres, skal den have et file object som argument.
+
+    Opgave 2:
+        I opgave 2 skal man bruge metoderne readint32bits()
+        og readbit() fra klassen BitReader fra det udleverede bibliotek bitIO.py til at læse heltal (for hyppighedstabel) og bits (for
+        Huffmans-koderne) fra inputfilen (den komprimerede fil). Man skal
+        bruge kaldet write(bytes([b])) (hvor write() er fra file objects
+        og bytes() er en built-in funktion) til skrive bytes til outputfilen
+        (den genskabte originale fil). Her er b et heltal som repræsenterer
+        den byte, som skal skrives. Begge filer skal ˚abnes i “binary mode”.
+        Når en BitReader instantieres, skal den have et file object som
+        argument.
+
+    :Gruppe medlemmer:
+        Mads Emil Falkenstrøm, mafal17@student.sdu.dk
+        Mathias Birkebjerg Kristiansen, matkr18@student.sdu.dk
+        Patrick Nielsen, panie18@student.sdu.dk
+"""
+
 from functools import total_ordering
 
 @total_ordering
--- huffman.py
+++ huffman.py
@@ -1,3 +1,31 @@
+""""
+    At lave dit eget værktøj til at komprimere filer.
+    Komprimeringen skal ske via Huffman-kodning. Der skal laves to
+    programmer: et til at kode/komprimere en fil, og et til dekode den igen.
+
+    Opgave 1:
+        I opgave 1 skal man bruge kaldet read(1) fra file objects til
+        at læse bytes fra inputfilen (den originale fil). Man skal bruge metoderne writeint32bits(intvalue) og writebit(bit) fra klassen BitWriter i biblioteket bitIO.py til at skrive heltal (for hyppighedstabel) og bits (for Huffmans-koderne) til outputfilen (den
+        komprimerede fil). Begge filer skal åbnes i “binary mode”. Når en
+        BitWriter instantieres, skal den have et file object som argument.
+
+    Opgave 2:
+        I opgave 2 skal man bruge metoderne readint32bits()
+        og readbit() fra klassen BitReader fra det udleverede bibliotek bitIO.py til at læse heltal (for hyppighedstabel) og bits (for
+        Huffmans-koderne) fra inputfilen (den komprimerede fil). Man skal
+        bruge kaldet write(bytes([b])) (hvor write() er fra file objects
+        og bytes() er en built-in funktion) til skrive bytes til outputfilen
+        (den genskabte originale fil). Her er b et heltal som repræsenterer
+        den byte, som skal skrives. Begge filer skal ˚abnes i “binary mode”.
+        Når en BitReader instantieres, skal den have et file object som
+        argument.
+
+    :Gruppe medlemmer:
+        Mads Emil Falkenstrøm, mafal17@student.sdu.dk
+        Mathias Birkebjerg Kristiansen, matkr18@student.sdu.dk
+        Patrick Nielsen, panie18@student.sdu.dk
+"""
+
 import PQHeap
 from Element import Element
 import bitIO
@@ -13,25 +41,40 @@ class Huffman:
         self.bitstreamout = bitIO.BitWriter(outfile)
 
     def make_frequency(self):
+        """
+            count the frequency of a byte in file and fill-out table
+
+            :return list table: the frequency table
+        """
         while True:
             x = self.infile.read(1)
             if not x:  # End-of-file?
                 break
             byte = ord(x)
-            bits = bin(byte)[2:].rjust(8, '0')
             self.table[byte] += 1
         self.infile.seek(0) # return file pointer to start of file
         return self.table
 
     def make_heap(self, frequency):
+        """
+            building the huffman tree structure
+
+            :param list frequency: the frequency table
+            :return list pq: The priority queue
+        """
         pq = []
         for key, freq in enumerate(frequency):
-            e = Element(freq, key)
-            PQHeap.insert(pq, e)
+            PQHeap.insert(pq, Element(freq, key))
         return pq
 
     def merge_nodes(self, pq):
-        while len(pq) > 1:
+        """
+            extracts the two minimum values, merges them and puts into the queue
+
+            :param list pq: The priority queue
+            :return int: the minimum value
+        """
+        while len(pq) > 1: # has elements in list
             left = PQHeap.extractMin(pq)
             right = PQHeap.extractMin(pq)
             z = Element(left.key + right.key, [left, right])
@@ -39,7 +82,17 @@ class Huffman:
         return PQHeap.extractMin(pq)
 
     def make_code(self, root, current_code=''):
-        if type(root.data) is int:
+        """
+            generates code path from root - of characters in the tree structure till leaf is hot
+            Traverse the tree structure using in-order traversal.
+            First traverse the left subtrees and append 0
+            Then traverse the right subtrees and append 1
+
+            :param Element root:
+            :param str current_code:
+            :return:
+        """
+        if type(root.data) is int: # leaf is hit
             self.codes[root.data] = current_code + '1'
             return
 
@@ -47,16 +100,30 @@ class Huffman:
         self.make_code(root.data[1], current_code + '1')
 
     def write_frequency(self, frequency):
+        """
+            writes the frequency table to the files first 32 bits
+
+            :param list frequency: the frequency table
+            :return:
+        """
         for i, freq in enumerate(frequency):
             self.bitstreamout.writeint32bits(int(freq))
 
     def read_frequency(self):
-        table = [0] * 256
-        for i in range(len(table)):
-            table[i] = self.bitstreamin.readint32bits()
-        return table
+        """
+            creates the frequency table from the files first 32 bits
+
+            :return list table: the frequency table
+        """
+        self.table = [self.bitstreamin.readint32bits() for _ in [0] * 256]
+        return self.table
 
     def compress(self):
+        """
+            compresses the input file
+
+            :return:
+        """
         frequency = self.make_frequency()
         pq = self.make_heap(frequency)
         root = self.merge_nodes(pq)
@@ -65,17 +132,22 @@ class Huffman:
 
         while True:
             x = self.infile.read(1)
-            if not x:
+            if not x: # End-of-file?
                 break
-            code = self.codes[ord(x)]
+            code = self.codes[ord(x)] # lookup code
             if code:
-                for bit in code:
+                for bit in code: # write each bit to file - right-fill with 0's
                     self.bitstreamout.writebit(int(bit))
 
         self.bitstreamin.close()
         self.bitstreamout.close()
 
     def decompress(self):
+        """
+            decompresses the compresses input file
+
+            :return:
+        """
         frequency = self.read_frequency()
         pq = self.make_heap(frequency)
         root = self.merge_nodes(pq)
@@ -84,12 +156,12 @@ class Huffman:
         element = root
         while total > 0:
             x = self.bitstreamin.readbit()
-            if type(element.data) is int:
+            if type(element.data) is int: # if leaf is hit
                 self.outfile.write(bytes([element.data]))
-                total = total - 1
+                total = total - 1 # bytes read
                 element = root
             else:
-                element = element.data[x]
+                element = element.data[x] # navigate through tree structure
 
         self.bitstreamin.close()
         self.bitstreamout.close()
